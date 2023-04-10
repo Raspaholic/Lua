6bac1134f2c100849e3c3c03683c5324009e709b
Raspaholic <tonyrasp31@gmail.com>
2023-04-10 16:17:20 -0500

    Section 4 complete + file restructure

---
+++ Section 4/incDecVar.lua
@@ -0,0 +1,10 @@
+level = 1
+score = 0
+num_lives = 5
+-- game running
+print("Level up!")
+level = level + 1
+print("Level: "..level)
+print("Missle hits you!")
+num_lives = num_lives - 1
+print("Lives: "..num_lives)
